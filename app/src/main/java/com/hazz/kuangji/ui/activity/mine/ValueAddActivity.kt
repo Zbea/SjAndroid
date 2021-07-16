304507a9946f7b740593d7e7495bc57e85ee8005
Zbea <xiaofeng9212@126.com>
2021-07-16 16:20:03 +0800

    增加增值服务

--- app/src/main/java/com/hazz/kuangji/ui/activity/mine/ValueAddActivity.kt
+++ app/src/main/java/com/hazz/kuangji/ui/activity/mine/ValueAddActivity.kt
@@ -1,5 +1,7 @@
 package com.hazz.kuangji.ui.activity.mine
 
+import android.content.Intent
+import android.util.Log
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hazz.kuangji.R
@@ -7,10 +9,14 @@ import com.hazz.kuangji.base.BaseActivity
 import com.hazz.kuangji.mvp.contract.IContractView
 import com.hazz.kuangji.mvp.model.ValueAdd
 import com.hazz.kuangji.mvp.presenter.ValueAddPresenter
+import com.hazz.kuangji.ui.activity.LoginActivity
 import com.hazz.kuangji.ui.adapter.ValueAddAdapter
-import com.hazz.kuangji.utils.ToolBarCustom
+import com.hazz.kuangji.utils.*
+import com.hazz.kuangji.widget.CommonDialog
 import com.hazz.kuangji.widget.RewardItemDeco
+import com.hazz.kuangji.widget.SafeCheckDialog
 import com.scwang.smartrefresh.layout.util.DensityUtil
+import kotlinx.android.synthetic.main.activity_home_rent.*
 import kotlinx.android.synthetic.main.activity_list.mToolBar
 import kotlinx.android.synthetic.main.activity_list.rc_list
 import kotlinx.android.synthetic.main.activity_value_add_page.*
@@ -20,16 +26,20 @@ import kotlinx.android.synthetic.main.fragment_mill.*
 class ValueAddActivity : BaseActivity(), IContractView.IValueAddView {
 
     private var mValueAddPresenter: ValueAddPresenter = ValueAddPresenter(this)
-    private var mAdapter:ValueAddAdapter?= null
+    private var mAdapter: ValueAddAdapter? = null
+    private var pos = 0
+    private var item: ValueAdd? = null
 
     override fun getList(item: ValueAdd) {
-        if (item==null)return
-        tv_money.text = item.amount+" FIL"
+        if (item == null) return
+        this.item = item
+        tv_money.text = item.amount + " FIL"
         mAdapter?.setNewData(item.list)
     }
 
     override fun onSuccess(msg: String) {
-        TODO("Not yet implemented")
+        mValueAddPresenter.getList()
+        SToast.showText(msg)
     }
 
     override fun layoutId(): Int {
@@ -41,22 +51,39 @@ class ValueAddActivity : BaseActivity(), IContractView.IValueAddView {
 
     override fun initView() {
         ToolBarCustom.newBuilder(mToolBar as Toolbar)
-                .setTitle("增值服务")
-                .setOnLeftIconClickListener {finish() }
+            .setTitle("增值服务")
+            .setOnLeftIconClickListener { finish() }
 
         rc_list.layoutManager = LinearLayoutManager(this)//创建布局管理
         mAdapter = ValueAddAdapter(R.layout.item_value_add, null)
         rc_list.adapter = mAdapter
         mAdapter?.bindToRecyclerView(rc_list)
+        mAdapter?.setEmptyView(R.layout.fragment_empty)
         val leftRightOffset = DensityUtil.dp2px(15f)
-        list.addItemDecoration(RewardItemDeco(0, 0, 0, leftRightOffset, 0))
+        rc_list.addItemDecoration(RewardItemDeco(0, 0, 0, leftRightOffset, 0))
+        mAdapter?.setOnItemChildClickListener { _, view, position ->
+            if (view.id == R.id.btn_commit) {
+                pos = position
+                SafeCheckDialog(this)
+                    .setCancelListener { }
+                    .setForgetListener {
+                        startActivity(Intent(this, ChangePwdActivity::class.java))
+                    }
+                    .setConfirmListener { _, password ->
+                        item?.list?.get(position)
+                            ?.let { mValueAddPresenter.extract(it?.id, Utils.encryptMD5(password)) }
+                    }.setCancelListener {
+
+                    }
+                    .show()
+            }
+        }
 
     }
+
     override fun start() {
         mValueAddPresenter.getList()
     }
-
-
 
 
 }
--- app/src/main/java/com/hazz/kuangji/ui/adapter/ValueAddAdapter.kt
+++ app/src/main/java/com/hazz/kuangji/ui/adapter/ValueAddAdapter.kt
@@ -1,6 +1,7 @@
 package com.hazz.kuangji.ui.adapter
 
 import android.view.View
+import android.widget.LinearLayout
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.hazz.kuangji.R
@@ -19,6 +20,21 @@ class ValueAddAdapter(layoutResId: Int, data: List<ValueAdd.ListBean>?) : BaseQu
         helper.setText(R.id.tv_date, item.createAt)
         helper.setText(R.id.tv_day, item.remain)
         helper.setText(R.id.tv_earnings, item.currentProfit+" FIL")
-        helper.setText(R.id.tv_state, if(item.status=="0")"进行中" else "已完成")
+        helper.setText(R.id.tv_state, if(item.status=="0")"进行中" else "已完成,待提取")
+        var isOpen=false
+        helper.getView<LinearLayout>(R.id.ll_click).setOnClickListener {
+            if (isOpen)
+            {
+                isOpen=false
+                helper.setVisible(R.id.ll_content,false)
+                helper.setImageResource(R.id.iv_arrow,R.mipmap.icon_arrow_down)
+            }
+            else{
+                isOpen=true
+                helper.setVisible(R.id.ll_content,true)
+                helper.setImageResource(R.id.iv_arrow,R.mipmap.icon_arrow_up)
+            }
+        }
+        helper.addOnClickListener(R.id.btn_commit)
     }
 }
